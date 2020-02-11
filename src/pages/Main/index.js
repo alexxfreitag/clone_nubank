0134e23a3fd83d4023c59c7ddeb0f0f14484d91b
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-02-10 23:47:17 -0300

    finish animations

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -21,6 +21,7 @@ import {
 } from './styles';
 
 export default function Main() {
+  let offset = 0;
   const translateY = new Animated.Value(0);
 
   const animatedEvent = Animated.event(
@@ -36,13 +37,37 @@ export default function Main() {
     },
   );
 
-  function onHandlerStateChanged(event) {}
+  function onHandlerStateChanged(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const {translationY} = event.nativeEvent;
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
 
   return (
     <Container>
       <Header />
       <Content>
-        <Menu />
+        <Menu translateY={translateY} />
 
         <PanGestureHandler
           onGestureEvent={animatedEvent}
@@ -75,7 +100,7 @@ export default function Main() {
           </Card>
         </PanGestureHandler>
       </Content>
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
